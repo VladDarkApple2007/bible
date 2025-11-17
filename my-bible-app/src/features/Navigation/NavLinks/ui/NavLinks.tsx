2401725499156dcade132a0cec0d4017aee2ce1b
Vlad <vladnas01galanix@gmail.com>
2025-11-17 17:51:48 +0200

    feat(navigation): add links to navbar

--- my-bible-app/src/features/Navigation/NavLinks/ui/NavLinks.tsx
+++ my-bible-app/src/features/Navigation/NavLinks/ui/NavLinks.tsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router-dom';
 import styles from "./NavLinks.module.scss";
 
 
 export const NavLinks = () => {
   return (
     <ul className={styles.navList}>
-      <li>Библия</li>
-      <li>Планы</li>
-      <li>Видео</li>
+      <li>
+        <Link to="/bible">Библия</Link>
+      </li>
+      <li>
+        <Link to="/plans">Планы</Link>
+      </li>
+      <li>
+        <Link to="/video">Видео</Link>
+      </li>
     </ul>
 
   )
